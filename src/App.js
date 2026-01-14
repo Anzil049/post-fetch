3f28e79d524fe341c3fceb670aed11a6167a9db5
anzil049 <anzil049@gmail.com>
2026-01-14 14:19:03 +0530

    fix

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import PostList from "./components/PostList";
+
+function App() {
+  return (
+    <div className="app">
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+function PostList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Post List</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostList;
